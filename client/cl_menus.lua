c5f43211fa721030ceff56a8a80022d27b11752a
xThrasherrr <thrashergamezz@gmail.com>
2024-08-07 00:46:59 -0600

    tweak: use ox_lib menu rather than context

--- client/cl_menus.lua
+++ client/cl_menus.lua
@@ -1,87 +1,88 @@
-local Config = require 'configs.client'
-
-function getDutyStr(state)
-    return (state == 1) and 'On Duty' or ' Off Duty'
-end
-
-function getDutyIcon(state)
-    return (state == 1) and Config.OnDutyIcon.icon or Config.OffDutyIcon.icon
-end
-
-function getDutyIconColor(state)
-    return (state == 1) and Config.OnDutyIcon.color or Config.OffDutyIcon.color
-end
+local lib           = lib
+local playerState   = LocalPlayer.state
 
 -- Other Employees Menu --
 local function otherEmployees(job)
-    local activeEmployees = lib.callback.await('xt-jobduty:server:getActiveEmployees', false, job)
+    local activeEmployees = lib.callback.await('xt-duty:server:getActiveEmployees', false, job)
     local menuOptions = {}
 
     if activeEmployees and activeEmployees[1] then
         for x = 1, #activeEmployees do
-            local dutyStr = ('Status: %s'):format(getDutyStr(activeEmployees[x].onDuty))
+            local dutyStr = getDutyStr(activeEmployees[x].onDuty)
             local dutyIcon = getDutyIcon(activeEmployees[x].onDuty)
             local dutyIconColor = getDutyIconColor(activeEmployees[x].onDuty)
             menuOptions[#menuOptions+1] = {
-                title = activeEmployees[x].name,
-                description = ('%s \n%s'):format(dutyStr, activeEmployees[x].clockInTime),
+                label = activeEmployees[x].name,
+                description = ('%s ▪ %s'):format(dutyStr, activeEmployees[x].clockInTime),
                 icon = dutyIcon,
                 iconColor = dutyIconColor,
+                close = false
             }
         end
     else
         menuOptions[#menuOptions+1] = {
-            title = 'No Other Employees',
+            label = 'No Other Employees',
         }
     end
 
-    lib.registerContext({
+    lib.registerMenu({
         id = 'duty_menu_employees',
         title = 'Other Employees',
-        menu = 'duty_menu',
-        options = menuOptions
+        position = 'top-right',
+        onClose = function()
+            lib.showMenu('duty_menu')
+        end,
+        options = menuOptions,
     })
-    lib.showContext('duty_menu_employees')
+    lib.showMenu('duty_menu_employees')
+end
+
+local function refreshDutyMenu()
+    local menu_id = lib.getOpenMenu()
+    if menu_id ~= 'duty_menu' then return end
+
+    lib.hideMenu()
+    Wait(500)
+    lib.setMenuOptions('duty_menu', {
+        label = 'Toggle Duty',
+        description = getDutyStr(playerState.onDuty),
+        checked = (playerState.onDuty and playerState.onDuty == 1),
+        icon = getDutyIcon(playerState.onDuty),
+        iconColor = getDutyIconColor(playerState.onDuty)}, 1)
+    lib.showMenu('duty_menu')
 end
 
 -- Main Duty Menu --
-local function dutyMenu()
-    local hasAllowedJob = allowedJobCheck()
-    local dutyStr = ('Status: %s'):format(getDutyStr(LocalPlayer.state.onDuty))
-    local dutyIcon = getDutyIcon(LocalPlayer.state.onDuty)
-    local dutyIconColor = getDutyIconColor(LocalPlayer.state.onDuty)
+local function dutyMenu(hasAllowedJob)
+    local dutyStr = getDutyStr(playerState.onDuty)
+    local dutyIcon = getDutyIcon(playerState.onDuty)
+    local dutyIconColor = getDutyIconColor(playerState.onDuty)
 
-    local menuOptions = {
-        {
-            title = 'Toggle Duty',
-            description = dutyStr,
-            icon = dutyIcon,
-            iconColor = dutyIconColor,
-            onSelect = function()
-                exports['xt-duty']:toggleDuty(hasAllowedJob)
-                dutyMenu()
-            end
-        },
-        {
-            title = 'View Active Employees',
-            icon = 'fas fa-people-group',
-            onSelect = function()
-                otherEmployees(hasAllowedJob)
-            end
-        }
-    }
-
-    lib.registerContext({
+    lib.registerMenu({
         id = 'duty_menu',
         title = 'Job Duty Menu',
-        options = menuOptions
-    })
-    lib.showContext('duty_menu')
+        position = 'top-right',
+        onCheck = function(selected, checked, args)
+            if selected == 1 then
+                exports['xt-duty']:toggleDuty(hasAllowedJob)
+                refreshDutyMenu()
+            end
+        end,
+        options = {
+            { label = 'Toggle Duty', description = dutyStr, icon = dutyIcon, iconColor = dutyIconColor, checked = (playerState.onDuty and playerState.onDuty == 1) },
+            { label = 'View Active Employees', icon = 'fas fa-people-group' },
+        }
+    }, function(selected, scrollIndex, args)
+        if selected == 2 then
+            otherEmployees(hasAllowedJob)
+        end
+    end)
+    lib.showMenu('duty_menu')
 end
 
 -- Open Duty Menu --
 RegisterCommand('duty', function()
     local hasAllowedJob = allowedJobCheck()
     if not hasAllowedJob then return end
-    dutyMenu()
+    dutyMenu(hasAllowedJob)
 end)
